d3c44d6860df2bd2cc87fd0c154255fc3eba1014
kenrique100 <ngwakenri2016@gmail.com>
2025-02-01 05:22:20 -0500

    full crud initialised

--- src/main/java/com/akentech/microservice/product/controller/ProductController.java
+++ src/main/java/com/akentech/microservice/product/controller/ProductController.java
@@ -1,18 +1,47 @@
 package com.akentech.microservice.product.controller;
 
+import com.akentech.microservice.product.dto.ProductRequest;
+import com.akentech.microservice.product.dto.ProductResponse;
+import com.akentech.microservice.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(){
+    public Mono<ProductResponse> createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Flux<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<ProductResponse> getProductById(@PathVariable String id) {
+        return productService.getProductById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<ProductResponse> updateProduct(@PathVariable String id, @RequestBody ProductRequest productRequest) {
+        return productService.updateProduct(id, productRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteProduct(@PathVariable String id) {
+        return productService.deleteProduct(id);
     }
 }
