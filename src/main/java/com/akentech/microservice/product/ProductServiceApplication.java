62a1c5590f4be08878b07717ee13a8af013fcc86
kenrique100 <ngwakenri2016@gmail.com>
2025-02-01 05:23:43 -0500

    updated reactive mongo

--- src/main/java/com/akentech/microservice/product/ProductServiceApplication.java
+++ src/main/java/com/akentech/microservice/product/ProductServiceApplication.java
@@ -2,12 +2,12 @@ package com.akentech.microservice.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories(basePackages = "com.akentech.microservice.product.repository")
 public class ProductServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
-
 }
